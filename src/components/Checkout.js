226b0e4f42337a10900a5d5915cb88981678c94c
Burak <mburak.demir.059@gmail.com>
2022-06-11 17:51:05 +0300

    Checkout Updates

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { initializeUseSelector } from 'react-redux/es/hooks/useSelector';
+import { initializeUseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
 
 export const Checkout = () => {
-  const cart=initializeUseSelector(state=>state.root.cartItems);
+  const cart=useSelector(state=>state.root.cartItems);
   return (
     <div>{
-         cart &&cart.map(item=>(
-          <div className="">{item.name}</div>
+         cart && cart.map(item=>(
+          <div className="cartItem" key={item.id}>
+
+              <img src={item.img} alt="" />
+              <div className="">
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+              </div>
+            </div>
         ))
       }</div>
   )
